888f3273cede756e074fa402c7a4de121da999a0
Anton Metlitsky <ticket1201@gmail.com>
2022-05-18 20:39:41 +0300

    refactor: add import Accordion from components, rating refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,61 +2,35 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Technologies from './Technologies';
+import Accordion from './components/Accordion/Accordion';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Technologies />
-      <div>
-        <Star />
-        <Star />
-        <Star />
-        <Star />
-        <Star />
-      </div>
-      <Accordion/>
+      <Rating />
+      <Accordion />
     </div>
   );
 }
 
+const Rating = () => {
+  return (
+    <div>
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+    </div>
+  )
+}
+
+
 const Star = () => {
   return (
     <div>Star</div>
-  )
-}
-
-const Accordion = () => {
-
-  console.log('Accordion rendering');
-  
-  return (
-    <div>
-      <AccordionTitle/>
-      <AccordionBody/>
-    </div>
-  )
-}
-
-
-const AccordionTitle = () => {
-  console.log('AccordionTitle rendering');
-  return(
-    <>This is accordion title text</>
-  )
-}
-
-
-const AccordionBody = () => {
-  console.log('AccordionBody rendering');
-  return(
-    <div>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-    </div>
   )
 }
 
