a486fcee5f193655dc9fc7f9e2e1c4343ce3dbfb
Anton Metlitsky <ticket1201@gmail.com>
2022-05-18 20:28:34 +0300

    feat: implement accodion components inc title and body

--- src/App.tsx
+++ src/App.tsx
@@ -15,15 +15,49 @@ const App = () => {
         <Star />
         <Star />
       </div>
+      <Accordion/>
     </div>
   );
 }
 
 const Star = () => {
-  return(
-    <div>Star</div> 
+  return (
+    <div>Star</div>
   )
 }
 
+const Accordion = () => {
+
+  console.log('Accordion rendering');
+  
+  return (
+    <div>
+      <AccordionTitle/>
+      <AccordionBody/>
+    </div>
+  )
+}
+
+
+const AccordionTitle = () => {
+  console.log('AccordionTitle rendering');
+  return(
+    <>This is accordion title text</>
+  )
+}
+
+
+const AccordionBody = () => {
+  console.log('AccordionBody rendering');
+  return(
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  )
+}
 
 export default App;
