b8d61a27607ad8c60c38aee2fecb273fbd200a5a
Anton Metlitsky <ticket1201@gmail.com>
2022-06-09 22:09:40 +0300

    refactor: rewrite to selfcontrol component

--- src/components/onOff/OnOFF.tsx
+++ src/components/onOff/OnOFF.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './OnOff.module.css'
 
-type OnOffType = {
-    isOn: boolean
-}
 
-export const OnOff = (props: OnOffType) => {
+export const OnOff = () => {
+    let [IsOn, setIsOn] = useState(false)
+
+
+
     return (
         <div className={s.lights}>
-            <div className={`${s.block} ${props.isOn && s.green}`}>ON</div>
-            <div className={`${s.block} ${!props.isOn && s.red}`}>Off</div>
-            {props.isOn && <div className={`${s.light} ${s.green}`}></div>}
-            {!props.isOn && <div className={`${s.light} ${s.red}`}></div>}
+            <div className={`${s.block} ${IsOn && s.green}`} onClick={() => {setIsOn(true)}}>ON</div>
+            <div className={`${s.block} ${!IsOn && s.red}`} onClick={() => {setIsOn(false)}}>Off</div>
+            {IsOn && <div className={`${s.light} ${s.green}`}></div>}
+            {!IsOn && <div className={`${s.light} ${s.red}`}></div>}
 
         </div>
     )
