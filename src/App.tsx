1ec08ff317ba20e88492fe2bb1e96fda37dc4feb
Anton Metlitsky <ticket1201@gmail.com>
2022-06-09 22:08:23 +0300

    feat: add SelfContrAccordion, UncontrolledRating, onOff components

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,35 @@ import Technologies from './Technologies';
 import Accordion from './components/Accordion/Accordion';
 import {Rating} from "./components/Rating/Rating";
 import { OmitProps } from 'antd/lib/transfer/ListBody';
+import {OnOff} from './components/onOff/OnOFF';
+import {SelfContrAccordion} from './components/SelfContrAccordion/SelfContrAccordion';
+import {UncontrolledRating} from './components/UncotrolledRating/UncontrolledRating';
 
 const App = () => {
     return (
         <div>
-            <PageTitle title = {'This is a first title'}/>
+          {/*  <PageTitle title = {'This is a first title'}/>
             <PageTitle title = {'This is a second title'}/>
             <Header/>
             Article 1
             <Technologies/>
-            <Rating value={3}/>
+            <UncontrolledRating value={3}/>
             <Accordion title = {'This is an Accordion title'} isCollapsed = {true}/>
             Article 2
             <Accordion title = {'This is an Accordion title2'} isCollapsed = {false}/>
+            <Rating value={0}/>
+            <Rating value={0}/>
             <Rating value={0}/>
             <Rating value={1}/>
             <Rating value={2}/>
             <Rating value={3}/>
             <Rating value={4}/>
-            <Rating value={5}/>
+            <Rating value={5}/>*/}
+            <SelfContrAccordion title={'menu'}/>
+            <SelfContrAccordion title={'users'}/>
+            <UncontrolledRating/>
+            <OnOff/>
+            <OnOff/>
         </div>
     );
 }
