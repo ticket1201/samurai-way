21764b2da0b3b50ffd5c267ba4b19127e0940e75
Anton Metlitsky <ticket1201@gmail.com>
2022-07-07 11:26:37 +0300

    feat: impl Reducer

---
+++ src/components/SelfContrAccordion/Reducer.tsx
@@ -0,0 +1,17 @@
+type ActionType = {
+    type: string
+
+}
+export type StateType = {
+    collapsed: boolean
+}
+
+export const reducer = (state: StateType, action: ActionType):StateType => {
+    /// change
+    switch (action.type) {
+        case 'TOGGLE-COLLAPSED':{
+            return {...state, collapsed: !state.collapsed};
+        }
+        default: throw new Error('Wrong action type')
+    }
+}
