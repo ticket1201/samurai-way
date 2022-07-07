b8ad486143a3b40b356c8fde0e43534b6a9ce19f
Anton Metlitsky <ticket1201@gmail.com>
2022-07-07 15:00:13 +0300

    refactor: rework to StoreContext.Consumer

--- src/components/Profile/MyPosts/MyPostsContainer.tsx
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import {storeType} from '../../../redux/store';
 import {addPostActionCreator, updateNewPostTextActionCreator} from '../../../redux/profile-reducer';
 import {MyPosts} from './MyPosts';
+import { StoreContext } from '../../../StoreContext';
 
 
 
-export const MyPostsContainer = (props: { store:storeType }) => {
-    let store = props.store.getState()
-    const addPost = () => {
-        props.store.dispatch(addPostActionCreator(store.profilePage.messageForNewPost))
-    }
-
-    const newTextChangeHandler = (newText:string) => {
-        props.store.dispatch(updateNewPostTextActionCreator(newText))
-    }
-
+export const MyPostsContainer = () => {
     return (
-     <MyPosts updateNewPostText={newTextChangeHandler} addPost={addPost} posts={store.profilePage.posts} newMessage={store.profilePage.messageForNewPost}/>
+        <StoreContext.Consumer>
+            {
+                (store) => {
+                    let state = store.getState()
+                    const addPost = () => {
+                        store.dispatch(addPostActionCreator(state.profilePage.messageForNewPost))
+                    }
+
+                    const newTextChangeHandler = (newText: string) => {
+                        store.dispatch(updateNewPostTextActionCreator(newText))
+                    }
+                    return (
+                        <MyPosts updateNewPostText={newTextChangeHandler} addPost={addPost}
+                                 posts={state.profilePage.posts}
+                                 newMessage={state.profilePage.messageForNewPost}/>
+                    )
+                }
+            }
+        </StoreContext.Consumer>
     )
 }
