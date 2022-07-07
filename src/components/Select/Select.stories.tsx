dc048b67dc1bec71ff35a990cfb19120edb4c013
Anton Metlitsky <ticket1201@gmail.com>
2022-07-07 11:28:30 +0300

    feat: impl custom select stories

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -5,13 +5,19 @@ export default {
     title: 'Select',
 };
 
-export const ModeChanging = () => {
+export const WithValue = () => {
     const [value, setValue] = useState<any>(1);
-    const onChange = (e:React.MouseEvent<HTMLDivElement>) => {
-      setValue(e.currentTarget.textContent)
-    }
+
 
     const items = [{title:'leha', value:1}, {title:'serega', value:2}, {title:'valera',value:3}]
 
-    return <Select items={items}  onChange={onChange} value={value}/>
+    return <Select items={items}  onChange={setValue} value={value}/>
+}
+
+export const WithoutValue = () => {
+    const [value, setValue] = useState<any>(null);
+
+    const items = [{title:'leha', value:1}, {title:'serega', value:2}, {title:'valera',value:3}]
+
+    return <Select items={items}  onChange={setValue} value={value}/>
 }
