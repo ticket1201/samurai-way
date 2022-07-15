8f8e06a459b09a8b4f17ffb7c7111720aa9b1625
Anton Metlitsky <ticket1201@gmail.com>
2022-07-15 22:05:02 +0300

    feat: implement types, initial state, dispatch cases

--- src/redux/dialogs-reducer.ts
+++ src/redux/dialogs-reducer.ts
@@ -1,4 +1,20 @@
-import {ActionsTypes, DialogsPageType} from './store';
+import {v1} from 'uuid';
+
+export type DialogsPageType = {
+    names: NamesItemType[]
+    messages: MessageItemType[]
+    newMessageText: string
+}
+type NamesItemType = {
+    id: string
+    name: string
+}
+type MessageItemType = {
+    id: string
+    message: string
+    time: string
+}
+
 
 type UpdateNewMessageTextActionType = {
     type: 'UPDATE_NEW_MESSAGE_TEXT'
@@ -7,48 +23,49 @@ type UpdateNewMessageTextActionType = {
 type SendMessageTextActionType = {
     type: 'SEND_MESSAGE'
 }
-
 export type dialogsReducerActionsTypes = UpdateNewMessageTextActionType | SendMessageTextActionType
 
 const initialState: DialogsPageType = {
     names: [
-        {id: 1, name: 'Dimych'},
-        {id: 2, name: 'Sanya'},
-        {id: 3, name: 'Olya'},
-        {id: 4, name: 'Valera'},
-        {id: 5, name: 'Valera'},
-        {id: 6, name: 'Viktor'},
+        {id: v1(), name: 'Dimych'},
+        {id: v1(), name: 'Sanya'},
+        {id: v1(), name: 'Olya'},
+        {id: v1(), name: 'Valera'},
+        {id: v1(), name: 'Valera'},
+        {id: v1(), name: 'Viktor'},
     ],
     messages: [
-        {id: 1, message: 'Hey', time: '13:15'},
-        {id: 2, message: 'Wats up?', time: '13:20'},
-        {id: 3, message: '????', time: '13:30'},
+        {id: v1(), message: 'Hey', time: '13:15'},
+        {id: v1(), message: 'Wats up?', time: '13:20'},
+        {id: v1(), message: '????', time: '13:30'},
     ],
     newMessageText: ''
 }
 
-export const dialogsReducer = (state = initialState, action: ActionsTypes) => {
+export const dialogsReducer = (state = initialState, action: dialogsReducerActionsTypes):DialogsPageType => {
     switch (action.type) {
         case 'UPDATE_NEW_MESSAGE_TEXT':
-            state.newMessageText = action.text;
-            return state
+            return {...state, newMessageText: action.text}
         case 'SEND_MESSAGE':
-            const message = state.newMessageText
-            state.newMessageText = ''
-            state.messages = [...state.messages, {
-                id: 6,
-                message: message,
-                time: `${new Date().getHours()}:${new Date().getMinutes()}`
-            }]
-            return state
+            return {
+                ...state,
+                messages: [
+                    ...state.messages,
+                    {
+                        id: v1(),
+                        message: state.newMessageText,
+                        time: `${new Date().getHours()}:${new Date().getMinutes()}`
+                    }],
+                newMessageText: ''
+            }
         default:
             return state
     }
 }
 
 export const sendMessageActionCreator = (): SendMessageTextActionType => {
-    return {type: 'SEND_MESSAGE'}
+    return {type: 'SEND_MESSAGE'} as const
 }
 export const updateNewMessageTextActionCreator = (message: string): UpdateNewMessageTextActionType => {
-    return {type: 'UPDATE_NEW_MESSAGE_TEXT', text: message}
+    return {type: 'UPDATE_NEW_MESSAGE_TEXT', text: message} as const
 }
