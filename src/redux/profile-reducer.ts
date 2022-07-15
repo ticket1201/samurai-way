3096cec5f19884fee1af4a5c288a6c8ca01eb605
Anton Metlitsky <ticket1201@gmail.com>
2022-07-15 22:07:32 +0300

    feat: add profile-reducer and results of reducers types

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -1,54 +1,55 @@
-import {ActionsTypes, PostItemStateType, PostsType} from './store';
+import {v1} from 'uuid';
 
+export type PostsType = PostItemStateType[]
+export type PostItemStateType = {
+    id: string
+    message: string
+    likeCount: number
+}
+export type ProfileStateType = {
+    posts: PostsType
+    messageForNewPost: string
+}
 
 type AddPostActionType = {
     type: 'ADD-POST'
-    postText: string
 }
 type UpdateNewTextActionType = {
     type: 'UPDATE-NEW-POST-TEXT'
     newText: string
 }
-export type profileReducerActionsTypes = AddPostActionType | UpdateNewTextActionType
-type ProfileReducerStateType = {
-    posts: PostsType
-    messageForNewPost: string
-}
 
-const initialState: ProfileReducerStateType = {
+export type profileReducerActionsTypes = AddPostActionType | UpdateNewTextActionType
+
+const initialState: ProfileStateType = {
     messageForNewPost: '',
     posts: [
-        {id: 1, message: 'Hi, how are you?', likeCount: 15},
-        {id: 2, message: 'It\'s my first post', likeCount: 1},
-        {id: 3, message: 'Hey ho lets go', likeCount: 1}
+        {id: v1(), message: 'Hi, how are you?', likeCount: 15},
+        {id: v1(), message: 'It\'s my first post', likeCount: 1},
+        {id: v1(), message: 'Hey ho lets go', likeCount: 1}
     ]
 }
 
 
-export const profileReducer = (state = initialState, action: ActionsTypes) => {
+export const profileReducer = (state = initialState, action: profileReducerActionsTypes): ProfileStateType => {
     switch (action.type) {
         case 'ADD-POST':
-            let newPost: PostItemStateType = {
-                id: 5,
-                message: action.postText,
+            const newPost: PostItemStateType = {
+                id: v1(),
+                message: state.messageForNewPost,
                 likeCount: 0
             }
-            state.posts = [newPost, ...state.posts]
-            state.messageForNewPost = '';
-            return state
+            return {...state, posts: [newPost, ...state.posts], messageForNewPost: ''}
         case 'UPDATE-NEW-POST-TEXT':
-            state.messageForNewPost = action.newText;
-            return state
+            return {...state, messageForNewPost: action.newText}
         default :
             return state
     }
-
 }
 
-export const addPostActionCreator = (postText: string): AddPostActionType => {
+export const addPostActionCreator = (): AddPostActionType => {
     return {
-        type: 'ADD-POST',
-        postText: postText
+        type: 'ADD-POST'
     }
 }
 export const updateNewPostTextActionCreator = (text: string): UpdateNewTextActionType => {
