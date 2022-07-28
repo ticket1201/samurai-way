44896510ee5d9bba02324f5023ab18960a6c9b94
Anton Metlitsky <ticket1201@gmail.com>
2022-07-28 23:42:48 +0300

    feat: add pagination, funcs for componentDidMount, onPageChanged.

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -8,15 +8,33 @@ import defaultAvatar from './../../assets/images/default_avatar.png'
 export class Users extends React.Component<UsersPropsType> {
 
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                    this.props.setUsers(response.data.items)
+                    this.props.setTotalUsersCount(response.data.totalCount)
+                }
+            )
+    }
+
+    onPageChanged = (pageNumber:number) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
             .then(response =>
                 this.props.setUsers(response.data.items)
             )
     }
 
     render() {
+
+        let pagesCount = Math.ceil(this.props.totalUserCount / this.props.pageSize)
+        let pages:Array<number> = []
+
+        for (let i = 1; i <= pagesCount; i++){
+            pages = [...pages, i]
+        }
+
         return (
-            <div>
+            <>
                 <h2 className={s.pageTitle}>Users</h2>
                 <div className={s.usersWrapper}>{
                     this.props.users.map(el => <div key={el.id} className={s.user}>
@@ -39,7 +57,12 @@ export class Users extends React.Component<UsersPropsType> {
                     </div>)
                 }
                 </div>
-            </div>
+                <div className={s.pagination}>
+                    {pages.map( p => {
+                       return <span className={this.props.currentPage === p ? s.selected : ''} onClick={ () =>  this.onPageChanged(p) }>{p}</span>
+                    })}
+                </div>
+            </>
         );
     }
 }
