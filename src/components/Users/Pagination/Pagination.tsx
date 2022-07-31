e516e8c850f6513fdd98eed1561a0829ed52e8f2
Anton Metlitsky <ticket1201@gmail.com>
2022-07-31 17:08:33 +0300

    feat: implement Pagination component

---
+++ src/components/Users/Pagination/Pagination.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import s from '../users.module.scss';
+
+
+type PaginationPropsType = {
+    totalUserCount: number
+    pageSize: number
+    currentPage: number
+    onPageChanged: (pageNumber:number) => void
+}
+
+export const Pagination = (props: PaginationPropsType) => {
+
+    let pagesCount = Math.ceil(props.totalUserCount / props.pageSize)
+    let pages: Array<number> = []
+
+    for (let i = 1; i <= pagesCount; i++) {
+        pages = [...pages, i]
+    }
+
+    return (
+        <div className={s.pagination}>
+            {pages.map(p => {
+                return <span className={props.currentPage === p ? s.selected : ''}
+                             onClick={() => props.onPageChanged(p)}>{p}</span>
+            })}
+        </div>
+    )
+};
+
