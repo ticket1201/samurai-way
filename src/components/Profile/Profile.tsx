8bb3a6e389a4c8c0600a67ba7b36af8b5004ce3f
Anton Metlitsky <ticket1201@gmail.com>
2022-07-31 17:09:07 +0300

    feat: add profileInfo, Contacts with props

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import styles from './Profile.module.scss'
 import {ProfileInfo} from './ProfileInfo/ProfileInfo';
 import {MyPostsContainer} from './MyPosts/MyPostsContainer';
+import {ProfilePageType} from '../../redux/profile-reducer';
+import Contacts from './Contacts/Contacts';
 
 
-export const Profile = () => {
+type ProfilePropsType = {
+    profile: ProfilePageType | null
+}
+
+export const Profile = (props: ProfilePropsType) => {
     return (
         <div className={styles.main}>
-            <ProfileInfo description={'Ava'} imgAlt={'Landscape'}
-                         imgUrl={'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'}/>
+            <ProfileInfo profile={props.profile}/>
+            {props.profile?.contacts && <Contacts profileContacts={props.profile.contacts}/>}
             <MyPostsContainer/>
-
         </div>
     )
 }
