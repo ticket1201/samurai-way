3ffad438c7f6c5fa5612dedd7c26d5cf0b9be6af
Anton Metlitsky <ticket1201@gmail.com>
2022-07-31 18:55:33 +0300

    feat: add ProlifeInfoLoader

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import defaultAvatar from './../../../assets/images/default_avatar.png'
 import s from './ProfileInfo.module.scss'
 import {ProfilePageType} from '../../../redux/profile-reducer';
+import ProfileInfoLoader from './ProfileInfoLoader/ProfileInfoLoader';
 
 type ProfileInfoType = {
     profile: ProfilePageType | null
@@ -12,7 +13,7 @@ export const ProfileInfo = (props: ProfileInfoType) => {
 
     if (!props.profile) {
         return (
-            <div>nothing</div>
+            <ProfileInfoLoader/>
         )
     }
 
@@ -24,7 +25,7 @@ export const ProfileInfo = (props: ProfileInfoType) => {
                 src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
                 alt="Landscape"/>
             <div className={s.profileMain}>
-                <img className={isLookingForJobStyle} src={props.profile.photos.small || defaultAvatar} alt="Avatar"/>
+                <img className={`${isLookingForJobStyle} ${s.image}`} src={props.profile.photos.small || defaultAvatar} alt="Avatar"/>
                 <div className={s.profileMainDescription}>
                     <h2>{props.profile.fullName}</h2>
                     <p>{props.profile.aboutMe}</p>
