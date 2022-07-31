67484e1b8c7739031dd768502578838a462e3149
Anton Metlitsky <ticket1201@gmail.com>
2022-07-31 17:06:04 +0300

    feat: add profile to init state, SET_USER_PROFILE action with creators and types

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -6,9 +6,33 @@ export type PostItemStateType = {
     message: string
     likeCount: number
 }
+export type ProfileContactsType = {
+    facebook: string
+    website: string
+    vk: string
+    twitter: string
+    instagram: string
+    youtube: string
+    github: string
+    mainLink: string
+}
+type ProfilePhotosType = {
+    small: string
+    large: string
+}
+export type ProfilePageType = {
+    aboutMe: string
+    contacts: ProfileContactsType
+    lookingForAJob: boolean
+    lookingForAJobDescription: string
+    fullName: string
+    userId: number
+    photos: ProfilePhotosType
+}
 export type ProfileStateType = {
     posts: PostsType
     messageForNewPost: string
+    profile: ProfilePageType | null
 }
 
 type AddPostActionType = {
@@ -18,8 +42,10 @@ type UpdateNewTextActionType = {
     type: 'UPDATE-NEW-POST-TEXT'
     newText: string
 }
+type setUserProfileActionType = ReturnType<typeof setUserProfile>
 
-export type profileReducerActionsTypes = AddPostActionType | UpdateNewTextActionType
+
+export type profileReducerActionsTypes = AddPostActionType | UpdateNewTextActionType | setUserProfileActionType
 
 const initialState: ProfileStateType = {
     messageForNewPost: '',
@@ -27,7 +53,8 @@ const initialState: ProfileStateType = {
         {id: v1(), message: 'Hi, how are you?', likeCount: 15},
         {id: v1(), message: 'It\'s my first post', likeCount: 1},
         {id: v1(), message: 'Hey ho lets go', likeCount: 1}
-    ]
+    ],
+    profile: null
 }
 
 
@@ -42,6 +69,8 @@ export const profileReducer = (state = initialState, action: profileReducerActio
             return {...state, posts: [newPost, ...state.posts], messageForNewPost: ''}
         case 'UPDATE-NEW-POST-TEXT':
             return {...state, messageForNewPost: action.newText}
+        case 'SET_USER_PROFILE':
+            return {...state, profile: {...action.profile}}
         default :
             return state
     }
@@ -54,4 +83,11 @@ export const addPostActionCreator = (): AddPostActionType => {
 }
 export const updateNewPostTextActionCreator = (text: string): UpdateNewTextActionType => {
     return {type: 'UPDATE-NEW-POST-TEXT', newText: text}
+}
+
+export const setUserProfile = (profile:ProfilePageType) => {
+    return{
+        type:'SET_USER_PROFILE',
+        profile
+    } as const
 }
