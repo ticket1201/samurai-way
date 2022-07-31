34c6f9fd5c9b7d68b2779521be89a3b8c3f2cc57
Anton Metlitsky <ticket1201@gmail.com>
2022-07-31 17:10:34 +0300

    refactor: replace Profile with ProfileContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.scss';
 import {Header} from './components/Header/Header';
-import {Profile} from './components/Profile/Profile';
 import {Route} from 'react-router-dom';
 import {Music} from './components/Music/Music';
 import {News} from './components/News/News';
 import {Settings} from './components/Settings/Settings';
 import {DialogsContainer} from './components/Dialogs/DialogsContainer';
-import {UsersContainer} from './components/Users/UsersContainer';
+import UsersContainer from './components/Users/UsersContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
 
 
 const App = () => {
@@ -17,7 +17,7 @@ const App = () => {
             <div className={'content'}>
                 <Route path={'/dialogs'} render={() => <DialogsContainer/>}/>
                 <Route path={'/profile'}
-                       render={() => <Profile/>}/>
+                       render={() => <ProfileContainer/>}/>
                 <Route path={'/users'}
                        render={() => <UsersContainer/>}/>
                 <Route path={'/news'} render={() => <News/>}/>
