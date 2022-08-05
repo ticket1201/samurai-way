e097809a6ba6c35e0adeb8b379196d2f3f0cd790
Anton Metlitsky <ticket1201@gmail.com>
2022-08-05 17:36:50 +0300

    feat: add follow logic to buttons

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,6 +5,7 @@ import {UsersType} from '../../redux/users-reducer';
 import {UserLoader} from './UserLoader/UserLoader';
 import {Pagination} from './Pagination/Pagination';
 import {NavLink} from 'react-router-dom';
+import axios from 'axios';
 
 type UsersPropsType = {
     follow: (userID: string) => void
@@ -34,8 +35,39 @@ export const Users = (props: UsersPropsType) => {
                                         <img src={el.photos.small || defaultAvatar} alt="Avatar"/>
                                     </NavLink>
                                     {el.followed
-                                        ? <button onClick={() => props.unfollow(el.id)}>Unfollow</button>
-                                        : <button onClick={() => props.follow(el.id)}>Follow</button>}
+                                        ? <button
+                                            onClick={() => {
+                                                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${el.id}`,{
+                                                    withCredentials: true,
+                                                    headers: {
+                                                        'API-KEY': '06a5051c-4a76-4b4c-b1bc-63ff9bd7a4be'
+                                                    }
+                                                })
+                                                    .then(response => {
+                                                        if (response.data.resultCode === 0) {
+                                                            props.unfollow(el.id)
+                                                        }
+                                                    })
+                                            }}>
+                                            Unfollow
+                                        </button>
+                                        : <button
+                                            onClick={() => {
+                                                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${el.id}`, {},{
+                                                    withCredentials: true,
+                                                    headers: {
+                                                        'API-KEY': '06a5051c-4a76-4b4c-b1bc-63ff9bd7a4be'
+                                                    }
+                                                })
+                                                    .then(response => {
+                                                            if (response.data.resultCode === 0) {
+                                                                props.follow(el.id)
+                                                            }
+                                                        }
+                                                    )
+                                            }}>
+                                            Follow
+                                        </button>}
                                 </div>
                                 <NavLink className={s.userInfo} to={`/profile/${el.id}`}>
                                     <div className={s.userTitle}>
@@ -49,7 +81,8 @@ export const Users = (props: UsersPropsType) => {
                                 </NavLink>
                             </div>)
                         }
-                        <Pagination pageSize={props.pageSize} onPageChanged={props.onPageChanged} currentPage={props.currentPage} totalUserCount={props.totalUserCount}/>
+                        <Pagination pageSize={props.pageSize} onPageChanged={props.onPageChanged}
+                                    currentPage={props.currentPage} totalUserCount={props.totalUserCount}/>
                     </>
                 }
             </div>
