fd3d71c4f108c73c840c0228cad3cde6bb6ad7e6
Anton Metlitsky <ticket1201@gmail.com>
2022-08-05 17:35:03 +0300

    feat: add props and NavLink to login

--- src/components/Header/Navbar/Navbar.tsx
+++ src/components/Header/Navbar/Navbar.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import s from './Navbar.module.scss'
 import {NavLink} from 'react-router-dom';
-
-/*type styleType = {
-    nav: string,
-    item: string,
-    item active: string
-}*/
+import {authType} from '../../../redux/auth-reducer';
 
 
-export const Nav = () =>{
+export const Nav = (props:authType) =>{
     return(
         <nav className={s.nav}>
             <NavLink to="/profile" className={s.item} activeClassName={s.active}>Profile</NavLink>
@@ -18,6 +13,8 @@ export const Nav = () =>{
             <NavLink to="/music" className={s.item} activeClassName={s.active}>Music</NavLink>
             <NavLink to="/users" className={s.item} activeClassName={s.active}>Users</NavLink>
             <NavLink to="/settings" className={s.item} activeClassName={s.active}>Settings</NavLink>
+            {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+
         </nav>
     )
 }
