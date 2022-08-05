5d5a375f11ab5fa1e1b86aaee76627a65c7c22af
Anton Metlitsky <ticket1201@gmail.com>
2022-08-05 18:15:35 +0300

    feat: implement api.ts

---
+++ src/api/api.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': '06a5051c-4a76-4b4c-b1bc-63ff9bd7a4be'
+    }
+})
+
+export const usersAPI = {
+    getUsers: (currentPage = 1, pageSize = 4) => {
+        return axiosInstance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data)
+    },
+    getAuth: () => {
+        return axiosInstance.get(`auth/me`).then(response => response.data)
+    },
+    followUser: (userID: String) => {
+        return axiosInstance.post(`follow/${userID}`).then(resolve => resolve.data)
+    },
+    unfollowUser: (userID: String) => {
+        return axiosInstance.delete(`follow/${userID}`).then(resolve => resolve.data)
+    }
+}
+
+
+
