7b0262401ebe319d8cea0f23ac13bf92b40d3c86
Anton Metlitsky <ticket1201@gmail.com>
2022-08-11 18:09:39 +0300

    feat: add middleware

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,8 +1,9 @@
-import {combineReducers, createStore} from 'redux';
+import {applyMiddleware, combineReducers, createStore} from 'redux';
 import {profileReducer} from './profile-reducer';
 import {dialogsReducer} from './dialogs-reducer';
 import {usersReducer} from './users-reducer';
 import {authReducer} from './auth-reducer';
+import thunkMiddleware from 'redux-thunk';
 
 export type AppStatType = ReturnType<typeof rootReducer>
 
@@ -15,7 +16,7 @@ const rootReducer = combineReducers({
 })
 
 
-export let store = createStore(rootReducer)
+export let store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
 // @ts-ignore
 window.store = store;
