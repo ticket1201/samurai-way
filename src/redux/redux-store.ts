1e5799f8b3585ed7b06aebcb1888511e58e5ec2e
Anton Metlitsky <ticket1201@gmail.com>
2022-08-21 19:05:56 +0300

    feat: add form reducer to root from redux-form lib

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -4,6 +4,7 @@ import {dialogsReducer} from './dialogs-reducer';
 import {usersReducer} from './users-reducer';
 import {authReducer} from './auth-reducer';
 import thunkMiddleware from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
 
 export type AppStateType = ReturnType<typeof rootReducer>
 
@@ -12,7 +13,8 @@ const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 
