d0dacf62b695df1a1da83c6b15f925be4d3b9834
Anton Metlitsky <ticket1201@gmail.com>
2022-08-21 19:04:08 +0300

    feat: implement login component with styles

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import LoginForm, {FormDataType} from './LoginForm/LoginForm';
+import s from './Login.module.scss'
+
+
 
 export const Login = () => {
+    const onSubmit = (formData:FormDataType) => {
+        console.log(formData)
+    }
+
     return (
-        <div>
-            Login
+        <div className={s.wrapper}>
+            <h1>Login</h1>
+            <LoginForm onSubmit={onSubmit}/>
         </div>
     );
 };
