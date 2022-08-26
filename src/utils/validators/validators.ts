4a1eb237fd7be72e5136e3d7c0fe701419d1b354
Anton Metlitsky <ticket1201@gmail.com>
2022-08-26 16:58:46 +0300

    feat: add types for validators

--- src/utils/validators/validators.ts
+++ src/utils/validators/validators.ts
@@ -1,11 +1,13 @@
-export const required = (value: string) => {
-    if (value) return
+export type FieldValidatorType = (value:string) => string | undefined
+
+
+export const required:FieldValidatorType = (value) => {
+    if (value && value.trim().length) return
     return 'Field required!'
 }
 
 
-
-export const maxLengthCreator = (maxLength: number) => (value: string) => {
+export const maxLengthCreator = (maxLength: number):FieldValidatorType => (value: string)=> {
     if (value && value.length <= maxLength) return
     return `Max length is ${maxLength} symbols!`
 }
