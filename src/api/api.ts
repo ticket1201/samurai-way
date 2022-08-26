a320bb08d2b64d916a778e789296009da5c341a5
Anton Metlitsky <ticket1201@gmail.com>
2022-08-26 17:06:08 +0300

    feat: create authAPI login, logout requests

--- src/api/api.ts
+++ src/api/api.ts
@@ -11,6 +11,12 @@ const axiosInstance = axios.create({
 export const authAPI = {
     getAuth: () => {
         return axiosInstance.get(`auth/me`).then(response => response.data)
+    },
+    login: (email: string, password: string, rememberMe:boolean = false) => {
+        return axiosInstance.post(`auth/login`, {email, password, rememberMe}).then(resolve => resolve.data)
+    },
+    logout: () => {
+        return axiosInstance.delete(`auth/login`)
     }
 }
 
