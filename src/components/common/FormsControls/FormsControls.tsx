d55d37e93e75abd0bbf7b606968b234b06762aba
Anton Metlitsky <ticket1201@gmail.com>
2022-08-26 17:00:16 +0300

    feat: impl universal form element, TextArea, Login components

--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -1,28 +1,40 @@
 import React from 'react';
 import s from './FormControls.module.scss'
+import {WrappedFieldProps} from 'redux-form';
+
+type FormElementPropsType = {
+    type: string
+    children: React.ReactNode
+    isError: boolean
+} & WrappedFieldProps
 
 
-
-
-export const TextArea = ({input, meta, ...props}:any) => {
-    const isError = meta.touched && meta.error
+const FormElement = ({meta, children, isError}:FormElementPropsType) => {
     return (
         <>
-            <textarea {...input} {...props} className={isError ? `${s.error} ${s.textarea}` : s.textarea}/>
-            {isError && <span className={s.errorText}>some error</span>}
+            {children}
+            {isError && <p className={s.errorText}>{meta.error}</p>}
         </>
+    );
+}
+
+
+export const TextArea = (props:FormElementPropsType) => {
+    return (
+        <FormElement {...props}>
+            <textarea {...props.input} className={s.textarea}/>
+        </FormElement>
     );
 };
 
 
-/*
-export const Input = ({input, meta, ...props}:any) => {
-    const isError = meta.touched && meta.error
+export const Input = (props: FormElementPropsType)=> {
+    const isError = {isError: props.meta.touched && props.meta.error}
     return (
-        <>
-            <textarea {...input} {...props} className={isError ? `${s.error} ${s.textarea}` : s.textarea}/>
-            {isError && <span className={s.errorText}>some error</span>}
-        </>
+        <FormElement {...props} {...isError}>
+           <input {...props.input} type={props.type} className={isError.isError ? `${s.error} ${s.input}` : s.input}/>
+        </FormElement>
     );
 };
-*/
+
+
