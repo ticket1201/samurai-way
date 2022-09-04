a87960e7ab967c83ab4fe3c6571177e302120e04
Anton Metlitsky <ticket1201@gmail.com>
2022-09-05 00:58:01 +0300

    feat: add AppActionsType,AppThunk, app reducer to rootReducer

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,19 +1,29 @@
 import {applyMiddleware, combineReducers, createStore} from 'redux';
-import {profileReducer} from './profile-reducer';
-import {dialogsReducer} from './dialogs-reducer';
-import {usersReducer} from './users-reducer';
-import {authReducer} from './auth-reducer';
-import thunkMiddleware from 'redux-thunk';
-import { reducer as formReducer } from 'redux-form';
+import {profileReducer, profileReducerActionsTypes} from './profile-reducer';
+import {dialogsReducer, dialogsReducerActionsTypes} from './dialogs-reducer';
+import {usersACsTypes, usersReducer} from './users-reducer';
+import {authReducer, authReducerActionsTypes} from './auth-reducer';
+import thunkMiddleware, {ThunkAction} from 'redux-thunk';
+import {reducer as formReducer} from 'redux-form';
+import {AppActionType, appReducer} from './app-reducer';
 
 export type AppStateType = ReturnType<typeof rootReducer>
+export type AppActionsType =
+    | AppActionType
+    | authReducerActionsTypes
+    | dialogsReducerActionsTypes
+    | profileReducerActionsTypes
+    | usersACsTypes
 
+
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppStateType, unknown, AppActionsType>
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    app: appReducer,
     form: formReducer
 })
 
