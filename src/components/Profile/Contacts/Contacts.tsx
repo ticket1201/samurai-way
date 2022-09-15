0445e67e1f0be2fb21a5d1b70a5e9f4c4f3592d7
Anton Metlitsky <ticket1201@gmail.com>
2022-09-16 01:11:58 +0300

    refactor: rework props with destructurisation, replace links with LinkDisplay component

--- src/components/Profile/Contacts/Contacts.tsx
+++ src/components/Profile/Contacts/Contacts.tsx
@@ -1,86 +1,26 @@
 import React from 'react';
 import s from './contacts.module.scss';
 import {ProfileContactsType} from '../../../redux/profile-reducer';
+import {LinkDisplay} from '../../common/LinkDisplay';
 
 type ContactsPropsType = {
     profileContacts: ProfileContactsType
 }
 
 
-const Contacts = (props: ContactsPropsType) => {
-
-    const linkCheck = (link: string) => {
-        let startArr: Array<string> = []
-        for (let i = 0; i < 8; i++) {
-            startArr = [...startArr, link[i]]
-        }
-        let startStr = startArr.join('');
-        if (startStr !== 'https://') {
-            return 'https://' + link
-        } else {
-            return link
-        }
-    }
-
+const Contacts = ({profileContacts}: ContactsPropsType) => {
     return (
         <div className={s.contacts}>
             <h3>Contacts:</h3>
             <div className={s.contactsLinks}>
-                {props.profileContacts.facebook &&
-                    <p>Facebook:
-                        <a href={linkCheck(props.profileContacts.facebook)} target={'_blank'} rel="noreferrer">
-                            Facebook
-                        </a>
-                    </p>
-                }
-                {props.profileContacts.website &&
-                    <p>Website:
-                        <a href={linkCheck(props.profileContacts.website)} target={'_blank'} rel="noreferrer">
-                            WebSite
-                        </a>
-                    </p>
-                }
-                {props.profileContacts.vk &&
-                    <p>VK:
-                        <a href={linkCheck(props.profileContacts.vk)} target={'_blank'} rel="noreferrer">
-                            VK
-                        </a>
-                    </p>
-                }
-                {props.profileContacts.twitter &&
-                    <p>Twitter:
-                        <a href={linkCheck(props.profileContacts.twitter)} target={'_blank'} rel="noreferrer">
-                            Twitter
-                        </a>
-                    </p>
-                }
-                {props.profileContacts.instagram &&
-                    <p>Instagram:
-                        <a href={linkCheck(props.profileContacts.instagram)} target={'_blank'} rel="noreferrer">
-                            Instagram
-                        </a>
-                    </p>
-                }
-                {props.profileContacts.youtube &&
-                    <p>Youtube:
-                        <a href={linkCheck(props.profileContacts.youtube)} target={'_blank'} rel="noreferrer">
-                            YouTube
-                        </a>
-                    </p>
-                }
-                {props.profileContacts.github &&
-                    <p>GitHub:
-                        <a href={linkCheck(props.profileContacts.github)} target={'_blank'} rel="noreferrer">
-                            GitHub
-                        </a>
-                    </p>
-                } {props.profileContacts.mainLink &&
-                <p>MainLink:
-                    <a href={linkCheck(props.profileContacts.mainLink)} target={'_blank'} rel="noreferrer">
-                        main link
-                    </a>
-                </p>
-            }
+                {profileContacts.facebook && <LinkDisplay link={profileContacts.facebook} name={'Facebook'}/>}
+                {profileContacts.website && <LinkDisplay link={profileContacts.website} name={'WebSite'}/>}
+                {profileContacts.vk && <LinkDisplay link={profileContacts.vk} name={'VK'}/>}
+                {profileContacts.twitter && <LinkDisplay link={profileContacts.twitter} name={'Twitter'}/>}
+                {profileContacts.instagram && <LinkDisplay link={profileContacts.instagram} name={'Instagram'}/>}
+                {profileContacts.youtube && <LinkDisplay link={profileContacts.youtube} name={'YouTube'}/>}
+                {profileContacts.github && <LinkDisplay link={profileContacts.github} name={'GitHub'}/>}
+                {profileContacts.mainLink && <LinkDisplay link={profileContacts.mainLink} name={'Main Link'}/>}
             </div>
         </div>
     );
