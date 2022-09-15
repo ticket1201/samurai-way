49fc0a431e822cc521ea57807bb0594cc2152c78
Anton Metlitsky <ticket1201@gmail.com>
2022-09-15 16:38:01 +0300

    refactor: rework component with Pure class component

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -5,22 +5,24 @@ import {MyPostsPropsType} from './MyPostsContainer';
 import AddPostForm, {AddPostFormDataType} from './AddPostForm/AddPostForm';
 
 
-export const MyPosts = (props: MyPostsPropsType) => {
+export class MyPosts extends React.PureComponent<MyPostsPropsType> {
+    render() {
 
-    const postsElements = props.posts
-        .map(post => <Post message={post.message} likeCount={post.likeCount} key={post.id}/>)
+        const postsElements = this.props.posts
+            .map(post => <Post message={post.message} likeCount={post.likeCount} key={post.id}/>)
 
-    const handleSubmit = ({newPostText}: AddPostFormDataType) => {
-        props.addPost(newPostText)
-    }
+        const handleSubmit = ({newPostText}: AddPostFormDataType) => {
+            this.props.addPost(newPostText)
+        }
 
-    return (
-        <div className={s.posts}>
-            <h3>My posts</h3>
-            <AddPostForm onSubmit={handleSubmit}/>
-            <div className={'posts'}>
-                {postsElements}
+        return (
+            <div className={s.posts}>
+                <h3>My posts</h3>
+                <AddPostForm onSubmit={handleSubmit}/>
+                <div className={'posts'}>
+                    {postsElements}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
