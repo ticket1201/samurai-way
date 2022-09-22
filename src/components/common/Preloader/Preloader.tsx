171c3c866598f52a82fed99248a1a33df6deb14f
Anton Metlitsky <ticket1201@gmail.com>
2022-09-22 22:01:10 +0300

    feat: implement preloader component

---
+++ src/components/common/Preloader/Preloader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Preloader = () => {
+    return (
+        <div className={'app-wrapper'} style={{justifyContent: 'center', alignItems:'center'}}>
+            <p>LOADING</p>
+        </div>
+    );
+};
+
+export default Preloader;
