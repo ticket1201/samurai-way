a49625bd06740388e62ef53a0669e27ccf9efea0
Anton Metlitsky <ticket1201@gmail.com>
2022-09-22 22:00:26 +0300

    feat: rework Pagination component, add btns for scroll pages in pagination

--- src/components/Users/Pagination/Pagination.tsx
+++ src/components/Users/Pagination/Pagination.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from '../users.module.scss';
 
 
@@ -6,10 +6,11 @@ type PaginationPropsType = {
     totalUserCount: number
     pageSize: number
     currentPage: number
+    portionSize?: number
     onPageChanged: (pageNumber: number) => void
 }
 
-export const Pagination = ({totalUserCount, pageSize, currentPage, onPageChanged}: PaginationPropsType) => {
+export const Pagination = ({totalUserCount, pageSize, currentPage, onPageChanged, portionSize = 12}: PaginationPropsType) => {
 
     let pagesCount = Math.ceil(totalUserCount / pageSize)
     let pages: Array<number> = []
@@ -18,12 +19,28 @@ export const Pagination = ({totalUserCount, pageSize, currentPage, onPageChanged
         pages = [...pages, i]
     }
 
+
+    let portionsCont =  Math.ceil(pagesCount / portionSize)
+    const [portionNumber, setPortionNumber] = useState<number>(Math.ceil(currentPage/portionSize))
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber * portionSize;
+
+
+
     return (
         <div className={s.pagination}>
-            {pages.map((p, i) => {
-                return <span key={i} className={currentPage === p ? s.selected : ''}
-                             onClick={() => onPageChanged(p)}>{p}</span>
-            })}
+            <button onClick={() => setPortionNumber(1)} disabled={portionNumber > 0 && portionNumber <= 1}>first</button>
+            <button onClick={() => setPortionNumber((p) => p - 1)} disabled={portionNumber > 0 && portionNumber <= 1}>pred</button>
+            <span className={s.numbers}>
+                 {pages
+                     .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+                     .map((p) => {
+                         return <span key={p} className={currentPage === p ? s.selected : ''}
+                                      onClick={() => onPageChanged(p)}>{p}</span>
+                     })}
+            </span>
+            <button onClick={() => setPortionNumber((p) => p + 1)} disabled={portionsCont === portionNumber}>next</button>
+            <button onClick={() => setPortionNumber(portionsCont)} disabled={portionsCont === portionNumber}>last</button>
         </div>
     )
 };
