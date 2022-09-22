f2626bfcdb8795aa36f29819d88eadf8265ff8e0
Anton Metlitsky <ticket1201@gmail.com>
2022-09-22 21:59:18 +0300

    feat: rework UserLoadingPage, del pagination

--- src/components/Users/UsersLoadingPage/UserLoadingPage.tsx
+++ src/components/Users/UsersLoadingPage/UserLoadingPage.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import {UserLoader} from './UserLoader/UserLoader';
-import {Pagination} from '../Pagination/Pagination';
-import {UsersPropsType} from '../Users';
 
-export const UserLoadingPage = (props:UsersPropsType) => {
+export const UserLoadingPage = () => {
     return (
         <>
             <UserLoader/>
             <UserLoader/>
             <UserLoader/>
             <UserLoader/>
-            <Pagination {...props}/>
         </>
     );
 };
