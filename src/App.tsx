70959142b20def26f0c9ca7f446a72b584663fd7
Anton Metlitsky <ticket1201@gmail.com>
2022-09-29 20:52:55 +0300

    refactor: rework app with connect functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
-import React, {ComponentType} from 'react';
-import {connect} from 'react-redux';
-import {compose} from 'redux';
-import {Redirect, Route, withRouter} from 'react-router-dom';
+import React from 'react';
+import {connect, Provider} from 'react-redux';
+import {HashRouter, Redirect, Route} from 'react-router-dom';
 import './App.scss';
-import {AppStateType} from './redux/redux-store';
+import {AppStateType, store} from './redux/redux-store';
 import {initializeAppTC} from './redux/app-reducer';
 import {Header} from './components/Header/Header';
 import Login from './components/Login/Login';
 import Preloader from './components/common/Preloader/Preloader';
+import UsersContainer from './components/Users/UsersContainer';
 
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
-const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
-
-
-type AppPropsType = ReturnType<typeof mapSateToProps> & {
-    initializeAppTC: () => void
-}
 
 class App extends React.Component<AppPropsType> {
     componentDidMount() {
@@ -34,46 +28,40 @@ class App extends React.Component<AppPropsType> {
             <div className={'app-wrapper'}>
                 <Header/>
                 <div className={'content'}>
-                    <Route path={'/'} render={() => {
-                        return (
-                            <React.Suspense fallback={<Preloader/>}>
-                                <Redirect to={'/profile'}/>
-                            </React.Suspense>)
-                    }}/>
-                    <Route path={'/dialogs'} render={() => {
-                        return (
-                            <React.Suspense fallback={<Preloader/>}>
-                                <DialogsContainer/>
-                            </React.Suspense>)
-                    }}/>
-                    <Route path={'/profile/:userId?'}
-                           render={() => {
-                               return (
-                                   <React.Suspense fallback={<Preloader/>}>
-                                       <ProfileContainer/>
-                                   </React.Suspense>)
-                           }}/>
-                    <Route path={'/users'}
-                           render={() => {
-                               return (
-                                   <React.Suspense fallback={<Preloader/>}>
-                                       <UsersContainer/>
-                                   </React.Suspense>)
-                           }}/>
+                    <Route path={'/'} exact render={() => <Redirect to={'/profile'}/>}/>
+                    <React.Suspense fallback={<Preloader/>}>
+                        <Route path={'/dialogs'} render={() => <DialogsContainer/>}/>
+                        <Route path={'/profile/:userId?'} render={() => <ProfileContainer/>}/>
+                    </React.Suspense>
+                    <Route path={'/users'} render={() => <UsersContainer/>}/>
                     <Route path={'/login'} render={() => <Login/>}/>
                 </div>
             </div>
 
         )
-            ;
     }
 }
 
-const mapSateToProps = (state: AppStateType) => ({
+type mapStateToPropsType = {
+    initialized: boolean
+}
+type mapDispatchToPropsType = {
+    initializeAppTC: () => void
+}
+type AppPropsType = mapStateToPropsType & mapDispatchToPropsType
+
+const mapStateToProps = (state: AppStateType): mapStateToPropsType => ({
     initialized: state.app.initialized
 })
 
-export default compose<ComponentType>(
-    withRouter,
-    connect(mapSateToProps, {initializeAppTC})
-)(App)
+const AppContainer = connect(mapStateToProps, {initializeAppTC})(App);
+
+const MainApp = () => {
+    return <HashRouter>
+        <Provider store={store}>
+            <AppContainer/>
+        </Provider>
+    </HashRouter>
+}
+
+export default MainApp
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,9 @@ import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
-import {HashRouter} from 'react-router-dom';
-import {store} from './redux/redux-store';
-import {Provider} from 'react-redux';
+import MainApp from './App';
 
 
-ReactDOM.render(
-    <HashRouter>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </HashRouter>,
-    document.getElementById('root'));
+ReactDOM.render(<MainApp/>, document.getElementById('root'));
 
 
