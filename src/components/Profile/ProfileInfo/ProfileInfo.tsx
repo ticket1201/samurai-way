11b4c42fce48ffd7074add2351e28c300bb196d9
Anton Metlitsky <ticket1201@gmail.com>
2022-09-29 20:54:12 +0300

    feat: implement functions for edit contacts, profile descriptions

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import defaultAvatar from './../../../assets/images/default_avatar.png'
 import s from './ProfileInfo.module.scss'
 import {ProfilePageType} from '../../../redux/profile-reducer';
 import ProfileInfoLoader from './ProfileInfoLoader/ProfileInfoLoader';
 import ProfileStatus from './ProfileStatus/ProfileStatus';
+import ProfileForm from './ProfileForm/ProfileForm';
 
 type ProfileInfoType = {
     profile: ProfilePageType | null
     status: string
     updateStatus: (status: string) => void
+    savePhoto: (file: File) => void
+    saveProfile: (data: ProfilePageType) => Promise<any>
+    isOwner: boolean
 }
 
 
-export const ProfileInfo = ({profile, status, updateStatus}: ProfileInfoType) => {
+export const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto, saveProfile}: ProfileInfoType) => {
+    let [editMode, setEditMode] = useState(false)
 
     if (!profile) {
         return (
@@ -21,6 +26,16 @@ export const ProfileInfo = ({profile, status, updateStatus}: ProfileInfoType) =>
     }
 
     const isLookingForJobStyle = profile?.lookingForAJob ? s.lookingForJob : '';
+    const onMainPhotoSelected = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length) {
+            savePhoto(e.target.files[0])
+        }
+    }
+    const onSubmit = (formData: ProfilePageType) => {
+        saveProfile(formData).then(() => {
+            setEditMode(false)
+        })
+    }
 
     return (
         <div className={s.profileInfo}>
@@ -30,11 +45,19 @@ export const ProfileInfo = ({profile, status, updateStatus}: ProfileInfoType) =>
             <div className={s.profileMain}>
                 <img className={`${isLookingForJobStyle} ${s.image}`} src={profile.photos.small || defaultAvatar}
                      alt="Avatar"/>
+                {isOwner && <input type={'file'} onChange={onMainPhotoSelected}/>}
                 <div className={s.profileMainDescription}>
-                    <h2>{profile.fullName}</h2>
-                    <p>{profile.aboutMe}</p>
-                    {profile?.lookingForAJob && <p>Ищу работу как: {profile?.lookingForAJobDescription}</p>}
-                    <ProfileStatus status={status} updateStatus={updateStatus}/>
+                    {!editMode
+                        ? <>
+                            <h2>{profile.fullName}</h2>
+                            <p>Looking for a job: {profile.lookingForAJob || 'no'}</p>
+                            <p>{profile.aboutMe}</p>
+                            {profile?.lookingForAJob && <p>Ищу работу как: {profile?.lookingForAJobDescription}</p>}
+                            <ProfileStatus status={status} updateStatus={updateStatus}/>
+                            {isOwner && <button onClick={() => setEditMode(true)}>edit</button>}
+                        </>
+                        : <ProfileForm initialValues={profile} onSubmit={onSubmit}/>
+                    }
                 </div>
             </div>
 
