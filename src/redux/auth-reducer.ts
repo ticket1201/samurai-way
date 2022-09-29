bd499458c8387a008612a87b3e0c80eddd6ad9d9
Anton Metlitsky <ticket1201@gmail.com>
2022-09-29 23:06:34 +0300

    feat: add captchaUrl to State, implement setCaptcha AC, getCaptchaUrl TC, and condition to check response code to show captcha

--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -1,11 +1,11 @@
 import {Dispatch} from 'redux';
-import {authAPI} from '../api/api';
+import {authAPI, securityAPI} from '../api/api';
 import {stopSubmit} from 'redux-form';
 import {AppThunk} from './redux-store';
 
 export type setUserDataActionCreatorType = ReturnType<typeof setAuthUserData>
 
-export type authReducerActionsTypes = setUserDataActionCreatorType;
+export type authReducerActionsTypes = setUserDataActionCreatorType | ReturnType<typeof setCaptcha>;
 
 
 export type authType = {
@@ -13,18 +13,21 @@ export type authType = {
     email: string | null
     login: string | null
     isAuth: boolean
+    captchaUrl?: string
 }
 
 const initialState: authType = {
     id: null,
     email: null,
     login: null,
-    isAuth: false
+    isAuth: false,
+    captchaUrl: ''
 }
 
 
 export const authReducer = (state = initialState, action: authReducerActionsTypes): authType => {
     switch (action.type) {
+        case 'SET_CAPTCHA':
         case 'SET_USER_DATA':
             return {
                 ...state,
@@ -44,6 +47,12 @@ export const setAuthUserData = ({id, email, login, isAuth}: authType) => {
         payload: {id, email, login, isAuth}
     } as const
 }
+export const setCaptcha = (captchaUrl: string) => {
+    return {
+        type: 'SET_CAPTCHA',
+        payload: {captchaUrl}
+    } as const
+}
 
 //THUNK
 export const getAuthUserData = () => (dispatch: Dispatch<authReducerActionsTypes>) => {
@@ -57,13 +66,15 @@ export const getAuthUserData = () => (dispatch: Dispatch<authReducerActionsTypes
         )
 }
 
-export const login = (email: string, password: string, rememberMe: boolean):AppThunk => (dispatch) => {
-    authAPI.login(email, password, rememberMe)
+export const login = (email: string, password: string, rememberMe: boolean, captcha: string): AppThunk => (dispatch) => {
+    authAPI.login(email, password, rememberMe, captcha)
         .then(response => {
                 if (response.resultCode === 0) {
-                   dispatch(getAuthUserData())
-                }
-                else {
+                    dispatch(getAuthUserData())
+                } else {
+                    if (response.resultCode === 10) {
+                        dispatch(getCaptchaUrl())
+                    }
                     let message = response.messages.length > 0 ? response.messages[0] : 'Some error';
                     dispatch(stopSubmit('login', {_error: message}))
                 }
@@ -76,6 +87,14 @@ export const logout = () => (dispatch: Dispatch<authReducerActionsTypes>) => {
                 if (response.data.resultCode === 0) {
                     dispatch(setAuthUserData({id: null, email: null, login: null, isAuth: false}))
                 }
+            }
+        )
+}
+export const getCaptchaUrl = () => (dispatch: Dispatch<authReducerActionsTypes>) => {
+    securityAPI.getCaptchaUrl()
+        .then(response => {
+                const captchaUrl = response.data.url
+                dispatch(setCaptcha(captchaUrl))
             }
         )
 }
